7f24b2e02f016af22fdeb203e30188a435e1eca5
Mai Vu Bao <maivubao08012002@gmail.com>
2025-02-13 14:17:13 +0700

    fix migration BE .NET 8.

--- EventMate_BE/EventMate_WebAPI/Program.cs
+++ EventMate_BE/EventMate_WebAPI/Program.cs
@@ -9,10 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EventMate"));
-
-});
+	options.UseSqlServer(builder.Configuration.GetConnectionString("EventMate")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
