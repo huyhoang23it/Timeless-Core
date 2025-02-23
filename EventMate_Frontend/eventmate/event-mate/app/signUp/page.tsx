09a2393ea744a96473d2651c6be9ed2fefd8592b
Quang Vinh <vinhnqhe161630@fpt.edu.vn>
2025-02-24 00:03:23 +0700

    validate password in signUp

--- EventMate_Frontend/eventmate/event-mate/app/signUp/page.tsx
+++ EventMate_Frontend/eventmate/event-mate/app/signUp/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
 import CheckOTPModal from "@/components/authen/CheckOTPModal";
 import { Button } from "@/components/common/button";
 import Input from "@/components/common/Input";
 import InputSecret from "@/components/common/InputSecret";
+import { validatePassword } from "@/lib/helpers";
 import { useLanguage } from "@/providers/LanguageProvider";
 import { AuthRepository } from "@/repositories/AuthRepository";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const SignUpPage = () => {
   const { t } = useLanguage();
@@ -19,14 +20,27 @@ const SignUpPage = () => {
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [isShowChecoOTPModal, setIsShowForgotPasswordModal] = useState<boolean>(false);
- 
+  const [checks, setChecks] = useState<{ [key: string]: boolean }>({
+    length: false,
+    uppercase: false,
+    number: false,
+    specialChar: false,
+    math: false
+  });
+
+  useEffect(() => {
+    setChecks(validatePassword(newPassword, confirmPassword));
+  }, [newPassword, confirmPassword]);
+
   const [token, setToken] = useState<string>('');
+
+
   const handleSignUp = async () => {
     try {
       setLoading(true);
       const res = await AuthRepository.createOTP(email, newPassword);
       if (!res.error) {
-      setToken(res.data); 
+        setToken(res.data);
         setIsShowForgotPasswordModal(true);
       }
     } catch (e) {
@@ -40,7 +54,6 @@ const SignUpPage = () => {
     >
       {/* Container chính với hiệu ứng scale */}
       <div
-
         className=" backdrop-blur-lg shadow-xl rounded-xl px-8 py-10 w-full max-w-md mx-4"
       >
         {/* Tiêu đề */}
@@ -83,11 +96,24 @@ const SignUpPage = () => {
           />
 
         </div>
-
+        <ul>
+          {[
+            { key: "length", message: t('authen:password-validation:length') },
+            { key: "uppercase", message: t('authen:password-validation:uppercase') },
+            { key: "number", message: t('authen:password-validation:number')  },
+            { key: "specialChar", message: t('authen:password-validation:specialChar') },
+            { key: "match", message: t('authen:password-validation:match') }
+          ].map((item) => (
+            <li key={item.key} style={{ color: checks[item.key] ? "green" : "red" }}>
+              {checks[item.key] ?  <CheckCircleIcon className="w-5 h-5 mr-2" /> :  <XCircleIcon className="w-5 h-5 mr-2" />} {item.message}
+            </li>
+          ))}
+        </ul>
 
         <Button
           className="w-full font-semibold text-white items-center justify-center"
           label={t('authen:continue')}
+          disabled={!Object.values(checks).every(Boolean)}
           isLoading={loading}
           onClickButton={handleSignUp}
         ></Button>
@@ -109,7 +135,7 @@ const SignUpPage = () => {
           isOpen: isShowChecoOTPModal,
           closeModal: () => setIsShowForgotPasswordModal(false),
           title: t('authen:otp-title'),
-         
+
         }}
       />
     </div>
