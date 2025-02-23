c0499666a91a9cea8becba6a4a8bba0c29847c5d
Quang Vinh <vinhnqhe161630@fpt.edu.vn>
2025-02-23 10:10:56 +0700

    do signUp mock function

--- EventMate_Frontend/eventmate/event-mate/translation/lang/index.tsx
+++ EventMate_Frontend/eventmate/event-mate/translation/lang/index.tsx
@@ -1,23 +1,23 @@
 import enCommons from "../lang/en/commons.json";
 import viCommons from "../lang/vi/commons.json";
-import enLogin from "../lang/en/login.json";
-import viLogin from "../lang/vi/login.json";
+import enAuth from "../lang/en/authen.json";
+import viAuth from "../lang/vi/authen.json";
 import enSuccess from "../lang/en/success.json";
 import viSuccess from "../lang/vi/success.json";
 
 export const resources = {
-    en: {
-        commons: enCommons,
-    login: enLogin,
+  en: {
+    commons: enCommons,
+    authen: enAuth,
     success: enSuccess,
-    },
-    vi: {
-      commons: 
-        viCommons,
-        login: viLogin,
-        success: viSuccess,
- 
-    }
-  };
-  
+  },
+  vi: {
+    commons:
+      viCommons,
+      authen: viAuth,
+    success: viSuccess,
+
+  }
+};
+
 
