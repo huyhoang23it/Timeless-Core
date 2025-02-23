1b577870d301e9a42fd76edef842b90c1d51da95
khiemvu21206 <khiemvu08@gmail.com>
2025-02-23 23:02:59 +0700

    ok
    ok

---
+++ EventMate_BE/EventMate_Service/Services/AWS_Service.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMate_Service.Services
+{
+    public class AWS_Service
+    {
+        private readonly IAmazonS3 _s3Client;
+
+        public FilesController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file, string bucketName, string? prefix)
+        {
+            var bucketExists = await Amazon.S3.Util.AmazonS3Util.DoesS3BucketExistV2Async(_s3Client, bucketName);
+            if (!bucketExists) return NotFound($"Bucket {bucketName} does not exist.");
+            var request = new PutObjectRequest()
+            {
+                BucketName = bucketName,
+                Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+                InputStream = file.OpenReadStream()
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            await _s3Client.PutObjectAsync(request);
+            return Ok($"File {prefix}/{file.FileName} uploaded to S3 successfully!");
+        }
+    }
+}
